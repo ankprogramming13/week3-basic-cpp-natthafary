9628104ee910827baf2e5d51466f2a8b25431abe
Unknown <farynanmonot@gmail.com>
2017-11-21 17:36:22 +0700

    ทำข้อ 1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,5 +4,8 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
+
+  cout << "Miss." << endl << "Nattha" << endl << "thapcharoen" << endl << "Fary" << endl << "female" << endl << "7 feb 1999" << endl << "watch a movie" << endl << "single";
+
   return 0;
 }
