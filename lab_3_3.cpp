c339a08c706f6d2037466ef2be583ab23ce70f89
Unknown <farynanmonot@gmail.com>
2017-11-24 14:01:38 +0700

    ข้อ 3

--- lab_3_3.cpp
+++ lab_3_3.cpp
@@ -4,12 +4,17 @@ using namespace std;
 
 int main() {
 
-  const float pi = 3.14315926536;
-  float r;
+   const float pi = 3.14315926536;
+    float r;
+    float h=15;
 
-  cin >> r;
+    cin >> r;
 
-  cout << "Area : " << pi * r * r;
+    cout << "Area : " << pi * r * r;
+
+    cout << "Area : " << 2 * pi * r;
+
+    cout << "Area : " << pi * r * r * h;
 
   return 0;
 }
